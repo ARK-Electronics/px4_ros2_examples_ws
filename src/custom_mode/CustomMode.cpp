a0951a79a7f2de9be5f43e84ab0cb14314bc1956
Patrik DOminik Pordi <ppordi@umd.edu>
2024-07-25 12:43:21 -0600

    Code style has been adjusted

--- src/custom_mode/CustomMode.cpp
+++ src/custom_mode/CustomMode.cpp
@@ -5,10 +5,10 @@
 
 #include "CustomMode.hpp"
 
-#include <px4_ros2/components/node_with_mode.hpp>
-#include <px4_ros2/utils/geometry.hpp>
 #include <Eigen/Core>
 #include <Eigen/Geometry>
+#include <px4_ros2/components/node_with_mode.hpp>
+#include <px4_ros2/utils/geometry.hpp>
 
 // Constants
 static const std::string kModeName = "CustomMode";
@@ -17,260 +17,267 @@ static const bool kEnableDebugOutput = true;
 using namespace px4_ros2::literals;
 
 // Constructor for the CustomMode class
-CustomMode::CustomMode(rclcpp::Node& node)
-	: ModeBase(node, kModeName)
-	, _node(node)
-{
-	// Initialize the trajectory setpoint and odometry local position
-	_trajectory_setpoint = std::make_shared<px4_ros2::TrajectorySetpointType>(*this);
-	_vehicle_local_position = std::make_shared<px4_ros2::OdometryLocalPosition>(*this);
+CustomMode::CustomMode(rclcpp::Node &node)
+    : ModeBase(node, kModeName), _node(node) {
+  // Initialize the trajectory setpoint and odometry local position
+  _trajectory_setpoint =
+      std::make_shared<px4_ros2::TrajectorySetpointType>(*this);
+  _vehicle_local_position =
+      std::make_shared<px4_ros2::OdometryLocalPosition>(*this);
 
-	// Subscribe to vehicle_land_detected
-	_vehicle_land_detected_sub = _node.create_subscription<px4_msgs::msg::VehicleLandDetected>("/fmu/out/vehicle_land_detected",
-			   rclcpp::QoS(1).best_effort(), std::bind(&CustomMode::vehicleLandDetectedCallback, this, std::placeholders::_1));
-	// Declare the trajectory type parameter with a default value of "circle"
-	_node.declare_parameter("trajectory_type", "circle");
-	_node.get_parameter("trajectory_type", _trajectory_type);
-	
+  // Subscribe to vehicle_land_detected
+  _vehicle_land_detected_sub =
+      _node.create_subscription<px4_msgs::msg::VehicleLandDetected>(
+          "/fmu/out/vehicle_land_detected", rclcpp::QoS(1).best_effort(),
+          std::bind(&CustomMode::vehicleLandDetectedCallback, this,
+                    std::placeholders::_1));
+  // Declare the trajectory type parameter with a default value of "circle"
+  _node.declare_parameter("trajectory_type", "circle");
+  _node.get_parameter("trajectory_type", _trajectory_type);
 }
 
-void CustomMode::vehicleLandDetectedCallback(const px4_msgs::msg::VehicleLandDetected::SharedPtr msg)
-{
-	_land_detected = msg->landed;
+void CustomMode::vehicleLandDetectedCallback(
+    const px4_msgs::msg::VehicleLandDetected::SharedPtr msg) {
+  _land_detected = msg->landed;
 }
 
 // OnActivate function generates waypoints and switches to the Execute state
-void CustomMode::onActivate()
-{
-	generateWaypoints();
-	switchToState(State::Execute);
+void CustomMode::onActivate() {
+  generateWaypoints();
+  switchToState(State::Execute);
 }
 // OnDeactivate function logs a message when the mode is deactivated
-void CustomMode::onDeactivate()
-{
-	RCLCPP_INFO(_node.get_logger(), "Deactivating CustomMode");
+void CustomMode::onDeactivate() {
+  RCLCPP_INFO(_node.get_logger(), "Deactivating CustomMode");
 }
 
 // UpdateSetpoint function updates the setpoint based on the current state
-void CustomMode::updateSetpoint(float dt_s)
-{
-	
-	// Switching between states
-	switch (_state) {
-	// Execute state is the main state where the drone follows the waypoints
-	case State::Execute: {
+void CustomMode::updateSetpoint(float dt_s) {
 
-		// Get the next waypoint
-		Eigen::Vector3f target_position = _waypoints[_waypoint_index];
+  // Switching between states
+  switch (_state) {
+  // Execute state is the main state where the drone follows the waypoints
+  case State::Execute: {
 
-		// Create a trajectory setpoint message
-		px4_msgs::msg::TrajectorySetpoint setpoint;
+    // Get the next waypoint
+    Eigen::Vector3f target_position = _waypoints[_waypoint_index];
 
-		// Construct the trajectory setpoint message
-		setpoint.timestamp = _node.now().nanoseconds() / 1000;
-		setpoint.position = { target_position.x(), target_position.y(), target_position.z() };
-		setpoint.velocity = { NAN, NAN, NAN };
-		setpoint.acceleration = { NAN, NAN, NAN} ;
-		setpoint.jerk = { NAN, NAN, NAN };
-		setpoint.yaw = NAN;
-		setpoint.yawspeed = NAN;
+    // Create a trajectory setpoint message
+    px4_msgs::msg::TrajectorySetpoint setpoint;
 
-		// Publish the trajectory setpoint
-		_trajectory_setpoint->update(setpoint);
+    // Construct the trajectory setpoint message
+    setpoint.timestamp = _node.now().nanoseconds() / 1000;
+    setpoint.position = {target_position.x(), target_position.y(),
+                         target_position.z()};
+    setpoint.velocity = {NAN, NAN, NAN};
+    setpoint.acceleration = {NAN, NAN, NAN};
+    setpoint.jerk = {NAN, NAN, NAN};
+    setpoint.yaw = NAN;
+    setpoint.yawspeed = NAN;
 
+    // Publish the trajectory setpoint
+    _trajectory_setpoint->update(setpoint);
 
-		// Check if the drone has reached the target position
-		if (positionReached(target_position)) {
-			// Move to the next waypoint
-			_waypoint_index++;
+    // Check if the drone has reached the target position
+    if (positionReached(target_position)) {
+      // Move to the next waypoint
+      _waypoint_index++;
 
-			// If we have Executeed all waypoints, go home
-			if (_waypoint_index >= static_cast<int>(_waypoints.size())) {
-				// Switch to state Home
-				switchToState(State::Home);
-			}
-		}
+      // If we have Executeed all waypoints, go home
+      if (_waypoint_index >= static_cast<int>(_waypoints.size())) {
+        // Switch to state Home
+        switchToState(State::Home);
+      }
+    }
 
-		break;
-	}
-	// Home state is the state where the drone returns to the X,Y position where the drone was armed
-	case State::Home: {
-		// Set the target position to the X,Y position where the drone was armed
-		Eigen::Vector3f target_position = { 0.0f, 0.0f, _vehicle_local_position->positionNed().z()};
-		// Create a trajectory setpoint message
-		px4_msgs::msg::TrajectorySetpoint setpoint;
-		// Construct the trajectory setpoint message
-		setpoint.timestamp = _node.now().nanoseconds() / 1000;
-		setpoint.position = { target_position.x(), target_position.y(), target_position.z() };
-		setpoint.velocity = { NAN, NAN, NAN };
-		setpoint.acceleration = { NAN, NAN, NAN };
-		setpoint.jerk = { NAN, NAN, NAN };
-		setpoint.yaw = NAN;
-		setpoint.yawspeed = NAN;
-		// Publish the trajectory setpoint
-		_trajectory_setpoint->update(setpoint);
-		// Check if the drone has reached the target position
-		if (positionReached(target_position)) {
-			// Switch to state Descend
-			switchToState(State::Descend);
-		}
+    break;
+  }
 
-		break;
+  // Home state is the state where the drone returns to the X,Y position where
+  // the drone was armed
+  case State::Home: {
+    // Set the target position to the X,Y position where the drone was armed
+    Eigen::Vector3f target_position = {
+        0.0f, 0.0f, _vehicle_local_position->positionNed().z()};
+    // Create a trajectory setpoint message
+    px4_msgs::msg::TrajectorySetpoint setpoint;
+    // Construct the trajectory setpoint message
+    setpoint.timestamp = _node.now().nanoseconds() / 1000;
+    setpoint.position = {target_position.x(), target_position.y(),
+                         target_position.z()};
+    setpoint.velocity = {NAN, NAN, NAN};
+    setpoint.acceleration = {NAN, NAN, NAN};
+    setpoint.jerk = {NAN, NAN, NAN};
+    setpoint.yaw = NAN;
+    setpoint.yawspeed = NAN;
+    // Publish the trajectory setpoint
+    _trajectory_setpoint->update(setpoint);
 
-	}
-	
+    // Check if the drone has reached the target position
+    if (positionReached(target_position)) {
+      // Switch to state Descend
+      switchToState(State::Descend);
+    }
 
-	
-	// Descend state is the state where the drone lands
-	case State::Descend: {
+    break;
+  }
 
-		// Create a trajectory setpoint message
-		px4_msgs::msg::TrajectorySetpoint setpoint;
+  // Descend state is the state where the drone lands
+  case State::Descend: {
 
-		// Construct the trajectory setpoint message
-		setpoint.timestamp = _node.now().nanoseconds() / 1000;
-		setpoint.position = {0.0, 0.0, NAN};
-		setpoint.velocity = {NAN, NAN, 0.35};
-		setpoint.acceleration = {NAN, NAN, NAN};
-		setpoint.jerk = {NAN, NAN, NAN};
-		setpoint.yaw = NAN;
-		setpoint.yawspeed = NAN;
-	
-		// Publish the trajectory setpoint
-		_trajectory_setpoint->update(setpoint);
-		// Check if the drone has landed
-		if (_land_detected) {
-			// Switch to state Finished
-			switchToState(State::Finished);
-		}
+    // Create a trajectory setpoint message
+    px4_msgs::msg::TrajectorySetpoint setpoint;
 
-		break;
-	}
-	// Finished state is the state where the mode is completed
-	case State::Finished: {
-		ModeBase::completed(px4_ros2::Result::Success);
-		break;
-	}
-	} // end switch/case
+    // Construct the trajectory setpoint message
+    setpoint.timestamp = _node.now().nanoseconds() / 1000;
+    setpoint.position = {0.0, 0.0, NAN};
+    setpoint.velocity = {NAN, NAN, 0.35};
+    setpoint.acceleration = {NAN, NAN, NAN};
+    setpoint.jerk = {NAN, NAN, NAN};
+    setpoint.yaw = NAN;
+    setpoint.yawspeed = NAN;
+
+    // Publish the trajectory setpoint
+    _trajectory_setpoint->update(setpoint);
+
+    // Check if the drone has landed
+    if (_land_detected) {
+      // Switch to state Finished
+      switchToState(State::Finished);
+    }
+
+    break;
+  }
+
+  // Finished state is the state where the mode is completed
+  case State::Finished: {
+    ModeBase::completed(px4_ros2::Result::Success);
+    break;
+  }
+  } // end switch/case
 }
 
 // GenerateWaypoints function generates waypoints based on the trajectory type
-void CustomMode::generateWaypoints()
-{
-	// Set the start position
-	double start_x = 0.0;
-	double start_y = 0.0;
-	double current_z = _vehicle_local_position->positionNed().z();
-	
-	// Set the maximum radius and number of points
-	double max_radius = 3.0;
-	int points = 16;
-	// Create a vector to store the waypoints
-	std::vector<Eigen::Vector3f> waypoints;
+void CustomMode::generateWaypoints() {
+  // Set the start position
+  double start_x = 0.0;
+  double start_y = 0.0;
+  double current_z = _vehicle_local_position->positionNed().z();
 
-	// Trajectory type circle
-	if (_trajectory_type == "circle") {
-		// Parameters for the circle pattern
-		double angle_increment = 2.0 * M_PI / points;
+  // Set the maximum radius and number of points
+  double max_radius = 3.0;
+  int points = 16;
+  // Create a vector to store the waypoints
+  std::vector<Eigen::Vector3f> waypoints;
 
-		// Generate circle waypoints
-		// The circle waypoints are generated in the NED frame
-		for (int point = 0; point < points; ++point) {
-			double angle = angle_increment * point;
-			double x = start_x + max_radius * cos(angle);
-			double y = start_y + max_radius * sin(angle);
-			double z = current_z;
-			// Push the waypoints to the vector
-			waypoints.push_back(Eigen::Vector3f(x, y, z));
-		}
+  // Trajectory type circle
+  if (_trajectory_type == "circle") {
+    // Parameters for the circle pattern
+    double angle_increment = 2.0 * M_PI / points;
 
-	}
-	// Trajectory type spiral
-	else if (_trajectory_type == "spiral") {
-		
-		// Parameters for the spiral pattern
-		double radius = 0.0;
-		// Generate spiral waypoints
-		// The spiral waypoints are generated in the NED frame
-		for (int point = 0; point < points + 1; ++point) {
-			double angle = 2.0 * M_PI * point / points;
-			double x = start_x + radius * cos(angle);
-			double y = start_y + radius * sin(angle);
-			double z = current_z;
-			// Push the waypoints to the vector
-			waypoints.push_back(Eigen::Vector3f(x, y, z));
-			// Increase the radius
-			radius += max_radius / points;
-		}
+    // Generate circle waypoints
+    // The circle waypoints are generated in the NED frame
+    for (int point = 0; point < points; ++point) {
+      double angle = angle_increment * point;
+      double x = start_x + max_radius * cos(angle);
+      double y = start_y + max_radius * sin(angle);
+      double z = current_z;
+      // Push the waypoints to the vector
+      waypoints.push_back(Eigen::Vector3f(x, y, z));
+    }
 
-		
-	}
-	// Trajectory type figure 8`
-	else if (_trajectory_type == "figure_8") {
-		// Parameters for the figure 8 pattern
-    	double angle_increment = 2.0 * M_PI / points;
-		// Generate figure 8 waypoints
-		// The figure 8 waypoints are generated in the NED frame
-    	for (int point = 0; point < points; ++point) {
-			double angle = angle_increment * point;
+  }
 
-			// Create the figure-8 pattern
-			double x = start_x + max_radius * sin(angle);
-			double y = start_y + max_radius * sin(2 * angle);
-			double z = current_z;  // Assuming constant z
-			// Push the waypoints to the vector
-			waypoints.push_back(Eigen::Vector3f(x, y, z));
-    	}
-	}
+  // Trajectory type spiral
+  else if (_trajectory_type == "spiral") {
 
-	// Set the Execute waypoints
-	_waypoints = waypoints;
+    // Parameters for the spiral pattern
+    double radius = 0.0;
+
+    // Generate spiral waypoints
+    // The spiral waypoints are generated in the NED frame
+    for (int point = 0; point < points + 1; ++point) {
+      double angle = 2.0 * M_PI * point / points;
+      double x = start_x + radius * cos(angle);
+      double y = start_y + radius * sin(angle);
+      double z = current_z;
+      // Push the waypoints to the vector
+      waypoints.push_back(Eigen::Vector3f(x, y, z));
+      // Increase the radius
+      radius += max_radius / points;
+    }
+
+  }
+
+  // Trajectory type figure 8`
+  else if (_trajectory_type == "figure_8") {
+    // Parameters for the figure 8 pattern
+    double angle_increment = 2.0 * M_PI / points;
+
+    // Generate figure 8 waypoints
+    // The figure 8 waypoints are generated in the NED frame
+    for (int point = 0; point < points; ++point) {
+      double angle = angle_increment * point;
+
+      // Create the figure-8 pattern
+      double x = start_x + max_radius * sin(angle);
+      double y = start_y + max_radius * sin(2 * angle);
+      double z = current_z; // Assuming constant z
+      // Push the waypoints to the vector
+      waypoints.push_back(Eigen::Vector3f(x, y, z));
+    }
+  }
+
+  // Set the Execute waypoints
+  _waypoints = waypoints;
 }
 // PositionReached function checks if the drone has reached the target position
-bool CustomMode::positionReached(const Eigen::Vector3f& target) const
-{
-	// Define the position and velocity thresholds
-	static constexpr float kDeltaPosition = 0.25f;
-	static constexpr float kDeltaVelocity = 0.25f;
+bool CustomMode::positionReached(const Eigen::Vector3f &target) const {
+  // Define the position and velocity thresholds
+  static constexpr float kDeltaPosition = 0.25f;
+  static constexpr float kDeltaVelocity = 0.25f;
 
-	// Get the current position and velocity
-	auto position = _vehicle_local_position->positionNed();
-	auto velocity = _vehicle_local_position->velocityNed();
+  // Get the current position and velocity
+  auto position = _vehicle_local_position->positionNed();
+  auto velocity = _vehicle_local_position->velocityNed();
 
-	// Calculate the delta position and velocity
-	const auto delta_pos = target - position;
-	
-	return (delta_pos.norm() < kDeltaPosition) && (velocity.norm() < kDeltaVelocity);
+  // Calculate the delta position and velocity
+  const auto delta_pos = target - position;
+
+  return (delta_pos.norm() < kDeltaPosition) &&
+         (velocity.norm() < kDeltaVelocity);
 }
 // StateName function returns the name of the state
-std::string CustomMode::stateName(State state)
-{
-	switch (state) {
-	case State::Home:
-		return "Home";
-	case State::Execute:
-		return "Execute";
-	case State::Descend:
-		return "Descend";
-	case State::Finished:
-		return "Finished";
-	default:
-		return "Unknown";
-	}
+std::string CustomMode::stateName(State state) {
+  switch (state) {
+  case State::Home:
+    return "Home";
+
+  case State::Execute:
+    return "Execute";
+
+  case State::Descend:
+    return "Descend";
+
+  case State::Finished:
+    return "Finished";
+
+  default:
+    return "Unknown";
+  }
 }
 // SwitchToState function switches to the specified state
-void CustomMode::switchToState(State state)
-{
-	RCLCPP_INFO(_node.get_logger(), "Switching to %s", stateName(state).c_str());
-	_state = state;
+void CustomMode::switchToState(State state) {
+  RCLCPP_INFO(_node.get_logger(), "Switching to %s", stateName(state).c_str());
+  _state = state;
 }
 
 // Main function
-int main(int argc, char* argv[])
-{
-	// Initialize the ROS2 node
-	rclcpp::init(argc, argv);
-	rclcpp::spin(std::make_shared<px4_ros2::NodeWithMode<CustomMode>>(kModeName, kEnableDebugOutput));
-	rclcpp::shutdown();
-	return 0;
+int main(int argc, char *argv[]) {
+  // Initialize the ROS2 node
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<px4_ros2::NodeWithMode<CustomMode>>(
+      kModeName, kEnableDebugOutput));
+  rclcpp::shutdown();
+  return 0;
 }
--- src/custom_mode/CustomMode.hpp
+++ src/custom_mode/CustomMode.hpp
@@ -5,149 +5,149 @@
 
 #pragma once
 
-#include <px4_ros2/components/mode.hpp>
-#include <px4_ros2/odometry/local_position.hpp>
-#include <px4_ros2/odometry/attitude.hpp>
 #include <px4_msgs/msg/trajectory_setpoint.hpp>
 #include <px4_msgs/msg/vehicle_land_detected.hpp>
+#include <px4_ros2/components/mode.hpp>
 #include <px4_ros2/control/setpoint_types/experimental/trajectory.hpp>
+#include <px4_ros2/odometry/attitude.hpp>
+#include <px4_ros2/odometry/local_position.hpp>
 
-
-#include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <cmath>
 #include <geometry_msgs/msg/pose.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
+#include <rclcpp/rclcpp.hpp>
 #include <vector>
 
 /**
- * @brief A custom mode class that demonstrates how to create a custom mode in PX4 ROS2
- * 
+ * @brief A custom mode class that demonstrates how to create a custom mode in
+ * PX4 ROS2
+ *
  */
-class CustomMode : public px4_ros2::ModeBase
-{
+class CustomMode : public px4_ros2::ModeBase {
 public:
-	/**
-	 * @brief Construct a new Custom Mode object
-	 * 
-	 * @param node The ROS2 node
-	 */
-	explicit CustomMode(rclcpp::Node& node);
+  /**
+   * @brief Construct a new Custom Mode object
+   *
+   * @param node The ROS2 node
+   */
+  explicit CustomMode(rclcpp::Node &node);
 
-	/**
-	 * @brief Callback for the vehicle_land_detected topic
-	 * 
-	 * @param msg The message
-	 */
-	void vehicleLandDetectedCallback(const px4_msgs::msg::VehicleLandDetected::SharedPtr msg);
+  /**
+   * @brief Callback for the vehicle_land_detected topic
+   *
+   * @param msg The message
+   */
+  void vehicleLandDetectedCallback(
+      const px4_msgs::msg::VehicleLandDetected::SharedPtr msg);
 
-	/**
-	 * @brief Called when the mode is activated
-	 * 
-	 */
-	void onActivate() override;
+  /**
+   * @brief Called when the mode is activated
+   *
+   */
+  void onActivate() override;
 
-	/**
-	 * @brief Called when the mode is deactivated
-	 * 
-	 */
-	void onDeactivate() override;
+  /**
+   * @brief Called when the mode is deactivated
+   *
+   */
+  void onDeactivate() override;
 
-	/**
-	 * @brief Update the setpoint
-	 * 
-	 * @param dt_s The time step
-	 */
-	void updateSetpoint(float dt_s) override;
+  /**
+   * @brief Update the setpoint
+   *
+   * @param dt_s The time step
+   */
+  void updateSetpoint(float dt_s) override;
 
 private:
-	/**
-	 * @brief Check if the drone has reached the target position
-	 * 
-	 * @param target The target position
-	 * @return true if the drone has reached the target position
-	 * @return false if the drone has not reached the target position
-	 */
-	bool positionReached(const Eigen::Vector3f& target) const;
+  /**
+   * @brief Check if the drone has reached the target position
+   *
+   * @param target The target position
+   * @return true if the drone has reached the target position
+   * @return false if the drone has not reached the target position
+   */
+  bool positionReached(const Eigen::Vector3f &target) const;
 
-	
-	/**
-	 * @brief Enum class for the state machine
-	 * 
-	 */
-	enum class State {
-		Home,		// Returns to the X,Y position where the drone was armed
-		Execute, 	// Executes the chosen pattern
-		Descend, 	// Lands the drone
-		Finished
-	};
+  /**
+   * @brief Enum class for the state machine
+   *
+   */
+  enum class State {
+    Home,    // Returns to the X,Y position where the drone was armed
+    Execute, // Executes the chosen pattern
+    Descend, // Lands the drone
+    Finished
+  };
 
-	/**
-	 * @brief Switch to a new state
-	 * 
-	 * @param state The new state
-	 */
-	void switchToState(State state);
-	/**
-	 * @brief Get the name of the state
-	 * 
-	 * @param state The state
-	 * @return std::string The name of the state
-	 */
-	std::string stateName(State state);
+  /**
+   * @brief Switch to a new state
+   *
+   * @param state The new state
+   */
+  void switchToState(State state);
+  /**
+   * @brief Get the name of the state
+   *
+   * @param state The state
+   * @return std::string The name of the state
+   */
+  std::string stateName(State state);
 
-	/**
-	 * @brief The ROS2 node
-	 * 
-	 */
-	rclcpp::Node& _node;
+  /**
+   * @brief The ROS2 node
+   *
+   */
+  rclcpp::Node &_node;
 
-	/**
-	 * @brief The subscription to the vehicle_land_detected topic
-	 * 
-	 */
-	rclcpp::Subscription<px4_msgs::msg::VehicleLandDetected>::SharedPtr _vehicle_land_detected_sub;
+  /**
+   * @brief The subscription to the vehicle_land_detected topic
+   *
+   */
+  rclcpp::Subscription<px4_msgs::msg::VehicleLandDetected>::SharedPtr
+      _vehicle_land_detected_sub;
 
-	/**
-	 * @brief The vehicle local position
-	 * 
-	 */
-	std::shared_ptr<px4_ros2::OdometryLocalPosition> _vehicle_local_position;
-	/**
-	 * @brief The trajectory setpoint
-	 * 
-	 */
-	std::shared_ptr<px4_ros2::TrajectorySetpointType> _trajectory_setpoint;
+  /**
+   * @brief The vehicle local position
+   *
+   */
+  std::shared_ptr<px4_ros2::OdometryLocalPosition> _vehicle_local_position;
+  /**
+   * @brief The trajectory setpoint
+   *
+   */
+  std::shared_ptr<px4_ros2::TrajectorySetpointType> _trajectory_setpoint;
 
-	/**
-	 * @brief The current state
-	 * 
-	 */
-	State _state = State::Execute;
+  /**
+   * @brief The current state
+   *
+   */
+  State _state = State::Execute;
 
-	/**
-	 * @brief Trajectory type parameter
-	 * 
-	 */
-	std::string _trajectory_type;
-	/**
-	 * @brief Waypoints for the search pattern
-	 * 
-	 */
-	std::vector<Eigen::Vector3f> _waypoints;
-	/**
-	 * @brief Generate waypoints for the pattern
-	 * 
-	 */
-	void generateWaypoints();
-	/**
-	 * @brief The current waypoint index
-	 * 
-	 */
-	int _waypoint_index = 0;
-	/**
-	 * @brief The landing status
-	 * 
-	 */
-	bool _land_detected = false;
+  /**
+   * @brief Trajectory type parameter
+   *
+   */
+  std::string _trajectory_type;
+  /**
+   * @brief Waypoints for the search pattern
+   *
+   */
+  std::vector<Eigen::Vector3f> _waypoints;
+  /**
+   * @brief Generate waypoints for the pattern
+   *
+   */
+  void generateWaypoints();
+  /**
+   * @brief The current waypoint index
+   *
+   */
+  int _waypoint_index = 0;
+  /**
+   * @brief The landing status
+   *
+   */
+  bool _land_detected = false;
 };
